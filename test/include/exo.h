eefa4fa4d0cfb07249fe42c430137f053b260092
MaxenceM <maxence.maurel0@gmail.com>
2025-02-11 09:41:57 +0100

    yes

---
+++ test/include/exo.h
@@ -0,0 +1,46 @@
+/**
+ * @file cours_test.h
+ * @brief Main header file for the cours_test project
+ * @author dev1ous
+ * @date 2025-01-19
+ */
+
+#pragma once
+
+ /**
+  * @brief Calculates the factorial of a number.
+  *
+  * @param n The non-negative integer for which to calculate the factorial
+  * @return int The factorial of n
+  * @throws std::overflow_error If the result exceeds the maximum value of int
+  *
+  * @note Time complexity: O(n)
+  * @warning Input must be non-negative
+  *
+  * @details
+  * The factorial of a non-negative integer n (written as n!) is the product of all
+  * positive integers less than or equal to n. For example:
+  * - 5! = 5 * 4 * 3 * 2 * 1 = 120
+  * - 0! is defined to be 1
+  *
+  * The function uses a recursive implementation.
+  *
+  * @code{.cpp}
+  * int result = factorial(5); // Returns 120
+  * int zero_factorial = factorial(0); // Returns 1
+  * @endcode
+  */
+
+#include <iostream>
+
+namespace MyFirstLib
+{
+	class MyMathClass
+	{
+	public:
+		static double Add(double a, double b);
+		static double Sub(double a, double b);
+		static double Mul(double a, double b);
+		static double Div(double a, double b);
+	};
+}
---
+++ test/src/exo.cpp
@@ -0,0 +1,21 @@
+#include "exo.h"
+
+double MyFirstLib::MyMathClass::Add(double a, double b)
+{
+    return a + b;
+}
+
+double MyFirstLib::MyMathClass::Sub(double a, double b)
+{
+    return a - b;
+}
+
+double MyFirstLib::MyMathClass::Mul(double a, double b)
+{
+    return a * b;
+}
+
+double MyFirstLib::MyMathClass::Div(double a, double b)
+{
+    return a / b;
+}
